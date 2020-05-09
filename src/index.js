0f30ba58484e058563654be7cdc83742d69ac2eb
ker tenepere <ker10epere@gmail.com>
2020-05-09 19:15:22 +0800

    refactored SeasonDisplay

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  state = { latitude: null, errorMessage: '', position: 'Loading...' }
+
+  state = { lat: null, errorMessage: '' }
+
   // Calls when apps started => Similar to constructor
   // Should contains only loading files
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => this.setState({ latitude: position.coords.latitude }),
+      (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
     )
   }
 
-  // Calls when State is updated
-  componentDidUpdate() {
-    console.log('Update')
-  }
-
-  componentWillUnmount() {
-    console.log('Unmount')
-  }
 
   render() {
-    if (!this.state.errorMessage) {
-      return (<div>
-        <h1>Latitude:{!this.state.latitude ? this.state.position : this.state.latitude} </h1><br />
-
-      </div>)
-    }
-    else {
+    if (this.state.errorMessage) {
       return <div>Error: {this.state.errorMessage}</div>
     }
+    if (this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />
+    }
+    return <div>Loading...</div>
   }
 }
 
